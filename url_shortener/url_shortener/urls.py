0ecaa58917682c65938ed0001d1dd715e0512828
etisamhaq <etisamulhaq2003@gmail.com>
2025-03-30 23:05:54 +0500

    adjust the urls in urls.py file

--- url_shortener/url_shortener/urls.py
+++ url_shortener/url_shortener/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for url_shortener project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from shortener.views import URLViewSet, redirect_to_original, get_url_stats
+
+router = DefaultRouter()
+router.register(r'urls', URLViewSet, basename='url')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('s/<str:short_code>/', redirect_to_original, name='redirect'),
+    path('api/stats/<str:short_code>/', get_url_stats, name='stats'),
 ]
